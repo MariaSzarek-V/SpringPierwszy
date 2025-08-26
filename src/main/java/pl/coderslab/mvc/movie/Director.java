a9dd2466246c46f58c6a3ed91e51577745a30892
MariaSzarek-V <mszarek@visa.com>
2025-08-26 16:29:01 +0200

    director movie super pomysl

---
+++ src/main/java/pl/coderslab/mvc/movie/Director.java
@@ -0,0 +1,22 @@
+package pl.coderslab.mvc.movie;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Director {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+}
---
+++ src/main/java/pl/coderslab/mvc/movie/Movie.java
@@ -0,0 +1,23 @@
+package pl.coderslab.mvc.movie;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @ManyToOne
+    private Director director;
+
+}
